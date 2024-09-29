7ca7ec7c24e9b8f3d3136d869e51c6f891d8821b
balibanirajukumar <rajukumar.balibani@outlook.com>
2024-09-28 22:33:57 -0500

    Update chatbot_streamlit.py

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -1,39 +1,74 @@
-import os
+from chatbot_chain import get_chatbot_chain
 import streamlit as st
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_openai import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory 
-import pandas as pd
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
+# Placeholder for storing user session state
+if "authentication_status" not in st.session_state:
+    st.session_state.authentication_status = None
 
-def get_chatbot_chain():
-    st.title("CSV Document Loader")
+# User credentials (in real apps, store them securely in a DB)
+hashed_passwords = stauth.Hasher(["abc", "def"]).generate()
 
-    # File uploader
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+with open("config.yaml") as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-    if uploaded_file is not None:
-        # Read the uploaded file as a pandas DataFrame
-        df = pd.read_csv(uploaded_file)
+authenticator = stauth.Authenticate(
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"],
+    config["preauthorized"],
+)
 
-        # Convert DataFrame to CSV and save to a temporary file
-        temp_file_path = "temp.csv"
-        df.to_csv(temp_file_path, index=False)
+name, authentication_status, username = authenticator.login()
 
-        # Load the CSV file using CSVLoader
-        loader = CSVLoader(file_path=temp_file_path)
-        documents = loader.load()
-
-        # Display the documents
-        st.write(documents)
-        vectorstore = FAISS.from_documents(documents, OpenAIEmbeddings())
-        memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-        chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0, model=os.environ.get("CHAT_GPT_MODEL"), streaming=True), retriever=vectorstore.as_retriever(), memory=memory)
-        return chain
-    else:
-        st.write("Please upload a CSV file.")
+if authentication_status:
+    authenticator.logout("Logout", "main")
+    if username == "raju":
+        st.write(f"Welcome *{name}*")
+    elif username == "rbriggs":
+        st.write(f"Welcome *{name}*")  
+    chain = get_chatbot_chain()
     
-    return None                                  
+elif authentication_status == False:
+    st.error("Username/password is incorrect")
+elif authentication_status == None:
+    st.warning("Please enter your username and password") 
+
+ 
+# URL for the logo of the assistant bot
+# We need it as a separate variable because it's used in multiple places
+bot_logo = 'https://pbs.twimg.com/profile_images/1739538983112048640/4NzIg1h6_400x400.jpg'
+
+# We use st.session_state and fill in the st.session_state.messages list
+# It's empty in the beginning, so we add the first message from the bot
+st.divider()
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [{"role": "bot",
+                                     "content": "Hello, how can I help?"}]
+ 
+# Then we show all the chat messages in Markdown format
+for message in st.session_state['messages']:
+    if message["role"] == 'bot':
+        with st.chat_message(message["role"], avatar=bot_logo):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+# We ask for the user's question, append it to the messages and show below
+if query := st.chat_input("Please ask your question here:"):
+    st.session_state.messages.append({"role": "user", "content": query})
+    with st.chat_message("user"):
+        st.markdown(query)
+ 
+    # We create a new chat message and launch the "chain" for the answer
+    with st.chat_message("assistant", avatar=bot_logo):
+        message_placeholder = st.empty()
+        result = chain.invoke({"question": query})
+        message_placeholder.markdown(result['answer'])
+ 
+    # We also add the answer to the messages history
+    st.session_state.messages.append({"role": "bot", "content": result['answer']})
